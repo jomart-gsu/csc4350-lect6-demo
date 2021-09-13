ddcd8b5a3d02c493d370a2a2f412a79ccfbf9df2
Ubuntu <azureuser@carl.5ejoayufn5iu3ehsfsvxvsyidc.bx.internal.cloudapp.net>
2021-09-13 23:01:19 +0000

    this is my commit

---
+++ lect6-demo.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+API_KEY = os.getenv("API_KEY")
+params = {
+    "q": "afghanistan",
+    "api-key": API_KEY
+}
+
+response = requests.get(
+    BASE_URL,
+    params=params,
+)
+
+response_json = response.json()
+
+try:
+    articles = response_json["response"]["docs"]
+    for article in articles:
+        print(article["headline"]["main"])
+except KeyError:
+    print("Couldn't fetch articles!")
+
